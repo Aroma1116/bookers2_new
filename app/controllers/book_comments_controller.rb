2b03887a05176a3c2621183f92c9df5b0e2f88d8
Aroma1116 <aroma1116@outlook.jp>
2022-07-09 12:07:35 +0000

    実装完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      flash[:notice] = "You have created comment successfully."
+    else
+      render 'books/show'
+    end
+  end
+
+  def destroy
+    BookComment.find(params[:id]).destroy
+    flash.now[:alert] = '投稿を削除しました'
+    @book = Book.find(params[:book_id])
   end
 
   private
